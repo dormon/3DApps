610e1e4fba7d26b9eeaae6879145cd6e8b52a74b
Tomáš Milet <amillhouse@seznam.cz>
2019-01-25 18:22:53 +0100

    Added linearInterpolationOfHomogenousCoords

---
+++ src/linearInterpolationOfHomogenousCoords.cpp
@@ -0,0 +1,205 @@
+#include <Simple3DApp/Application.h>
+#include <Vars/Vars.h>
+#include <geGL/StaticCalls.h>
+#include <geGL/geGL.h>
+#include <BasicCamera/FreeLookCamera.h>
+#include <BasicCamera/PerspectiveCamera.h>
+#include <BasicCamera/OrbitCamera.h>
+#include <Barrier.h>
+#include <imguiSDL2OpenGL/imgui.h>
+#include <imguiVars.h>
+#include <DrawGrid.h>
+#include <FreeImagePlus.h>
+#include<assimp/cimport.h>
+#include<assimp/scene.h>
+#include<assimp/postprocess.h>
+
+
+void createProgram(vars::Vars&vars){
+  if(notChanged(vars,"all",__FUNCTION__,{}))return;
+
+  std::string const vsSrc = R".(
+  uniform mat4 projection;
+  uniform mat4 view;
+  out vec2 vCoord;
+  void main(){
+    vec4 a = vec4(1,2,3,4);
+    vec4 b = vec4(1,1,3,0.1);
+    float t = float(gl_VertexID)/float(100);
+    vec4 x = a+(b-a)*t;
+    gl_Position = projection * view * x;
+  }
+  ).";
+  std::string const fsSrc = R".(
+  out vec4 fColor;
+  void main(){
+    fColor = vec4(1);
+  }
+  ).";
+
+  auto vs = std::make_shared<ge::gl::Shader>(GL_VERTEX_SHADER,
+      "#version 450\n",
+      vsSrc
+      );
+  auto fs = std::make_shared<ge::gl::Shader>(GL_FRAGMENT_SHADER,
+      "#version 450\n",
+      fsSrc
+      );
+  vars.reCreate<ge::gl::Program>("program",vs,fs);
+
+}
+
+void drawLIH(vars::Vars&vars){
+  createProgram(vars);
+  auto view = vars.getReinterpret<basicCamera::CameraTransform>("view");
+  auto projection = vars.get<basicCamera::PerspectiveCamera>("projection");
+  vars.get<ge::gl::Program>("program")
+    ->setMatrix4fv("view"      ,glm::value_ptr(view->getView()))
+    ->setMatrix4fv("projection",glm::value_ptr(projection->getProjection()))
+    ->use();
+  ge::gl::glDrawArrays(GL_POINTS,0,100);
+}
+
+class EmptyProject: public simple3DApp::Application{
+ public:
+  EmptyProject(int argc, char* argv[]) : Application(argc, argv) {}
+  virtual ~EmptyProject(){}
+  virtual void draw() override;
+
+  vars::Vars vars;
+
+  virtual void                init() override;
+  virtual void                mouseMove(SDL_Event const& event) override;
+  virtual void                key(SDL_Event const& e, bool down) override;
+  virtual void                resize(uint32_t x,uint32_t y) override;
+};
+
+void createView(vars::Vars&vars){
+  if(notChanged(vars,"all",__FUNCTION__,{"useOrbitCamera"}))return;
+
+  if(vars.getBool("useOrbitCamera"))
+    vars.reCreate<basicCamera::OrbitCamera>("view");
+  else
+    vars.reCreate<basicCamera::FreeLookCamera>("view");
+}
+
+void createProjection(vars::Vars&vars){
+  if(notChanged(vars,"all",__FUNCTION__,{"windowSize","camera.fovy","camera.near","camera.far"}))return;
+
+  auto windowSize = vars.get<glm::uvec2>("windowSize");
+  auto width = windowSize->x;
+  auto height = windowSize->y;
+  auto aspect = (float)width/(float)height;
+  auto nearv = vars.getFloat("camera.near");
+  auto farv  = vars.getFloat("camera.far" );
+  auto fovy = vars.getFloat("camera.fovy");
+
+  vars.reCreate<basicCamera::PerspectiveCamera>("projection",fovy,aspect,nearv,farv);
+}
+
+void createCamera(vars::Vars&vars){
+  createProjection(vars);
+  createView(vars);
+}
+
+void EmptyProject::init(){
+  vars.add<ge::gl::VertexArray>("emptyVao");
+  vars.addFloat("input.sensitivity",0.01f);
+  vars.add<glm::uvec2>("windowSize",window->getWidth(),window->getHeight());
+  vars.addFloat("camera.fovy",glm::half_pi<float>());
+  vars.addFloat("camera.near",.1f);
+  vars.addFloat("camera.far",1000.f);
+  vars.add<std::map<SDL_Keycode, bool>>("input.keyDown");
+  vars.addBool("useOrbitCamera",false);
+  createCamera(vars);
+}
+
+void EmptyProject::draw(){
+  createCamera(vars);
+  basicCamera::CameraTransform*view;
+
+  if(vars.getBool("useOrbitCamera"))
+    view = vars.getReinterpret<basicCamera::CameraTransform>("view");
+  else{
+    auto freeView = vars.get<basicCamera::FreeLookCamera>("view");
+    float freeCameraSpeed = 0.01f;
+    auto keys = vars.get<std::map<SDL_Keycode, bool>>("input.keyDown");
+    for (int a = 0; a < 3; ++a)
+      freeView->move(a, float((*keys)["d s"[a]] - (*keys)["acw"[a]]) *
+                            freeCameraSpeed);
+    view = freeView;
+  }
+
+  ge::gl::glClearColor(0.1f,0.1f,0.1f,1.f);
+  ge::gl::glClear(GL_COLOR_BUFFER_BIT);
+
+  vars.get<ge::gl::VertexArray>("emptyVao")->bind();
+
+  drawGrid(vars);
+  drawLIH(vars);
+
+  vars.get<ge::gl::VertexArray>("emptyVao")->unbind();
+
+  drawImguiVars(vars);
+
+  swap();
+}
+
+void EmptyProject::key(SDL_Event const& event, bool DOWN) {
+  auto keys = vars.get<std::map<SDL_Keycode, bool>>("input.keyDown");
+  (*keys)[event.key.keysym.sym] = DOWN;
+}
+
+void EmptyProject::mouseMove(SDL_Event const& e) {
+  if(vars.getBool("useOrbitCamera")){
+    auto sensitivity = vars.getFloat("input.sensitivity");
+    auto orbitCamera =
+        vars.getReinterpret<basicCamera::OrbitCamera>("view");
+    auto const windowSize     = vars.get<glm::uvec2>("windowSize");
+    auto const orbitZoomSpeed = 0.1f;//vars.getFloat("args.camera.orbitZoomSpeed");
+    auto const xrel           = static_cast<float>(e.motion.xrel);
+    auto const yrel           = static_cast<float>(e.motion.yrel);
+    auto const mState         = e.motion.state;
+    if (mState & SDL_BUTTON_LMASK) {
+      if (orbitCamera) {
+        orbitCamera->addXAngle(yrel * sensitivity);
+        orbitCamera->addYAngle(xrel * sensitivity);
+      }
+    }
+    if (mState & SDL_BUTTON_RMASK) {
+      if (orbitCamera) orbitCamera->addDistance(yrel * orbitZoomSpeed);
+    }
+    if (mState & SDL_BUTTON_MMASK) {
+      orbitCamera->addXPosition(+orbitCamera->getDistance() * xrel /
+                                float(windowSize->x) * 2.f);
+      orbitCamera->addYPosition(-orbitCamera->getDistance() * yrel /
+                                float(windowSize->y) * 2.f);
+    }
+  }else{
+    auto const xrel           = static_cast<float>(e.motion.xrel);
+    auto const yrel           = static_cast<float>(e.motion.yrel);
+    auto view = vars.get<basicCamera::FreeLookCamera>("view");
+    auto sensitivity = vars.getFloat("input.sensitivity");
+    if (e.motion.state & SDL_BUTTON_LMASK) {
+      view->setAngle(
+          1, view->getAngle(1) + xrel * sensitivity);
+      view->setAngle(
+          0, view->getAngle(0) + yrel * sensitivity);
+    }
+  }
+}
+
+void EmptyProject::resize(uint32_t x,uint32_t y){
+  auto windowSize = vars.get<glm::uvec2>("windowSize");
+  windowSize->x = x;
+  windowSize->y = y;
+  vars.updateTicks("windowSize");
+  ge::gl::glViewport(0,0,x,y);
+}
+
+
+int main(int argc,char*argv[]){
+  EmptyProject app{argc, argv};
+  app.start();
+  return EXIT_SUCCESS;
+}
