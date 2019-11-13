3406cec179f9454eaaa4d2d93f3fa7cf44420610
Tomáš Milet <amillhouse@seznam.cz>
2019-11-13 23:53:23 +0100

    Fixed CMakeLists.exe for emptyProject

--- src/heatUp.cpp
+++ src/heatUp.cpp
@@ -1,8 +1,8 @@
-#include <SDL.h>
-#include <geGL/geGL.h>
-#include <geGL/StaticCalls.h>
 #include<thread>
 #include<sstream>
+#include<vector>
+#include<iostream>
+#include<CL/cl.hpp>
 
 bool running = true;
 
@@ -11,8 +11,12 @@ void heatCPU(){
   }
 }
 
+void heatGPU(){
+
+}
+
 int main(int argc,char*argv[]){
-  size_t nofThreads = 8;
+  size_t nofThreads = argc>1?atoi(argv[1]):4;
   std::vector<std::thread>threads(nofThreads);
   for(size_t i=0;i<nofThreads;++i)
     threads.at(i) = std::thread(heatCPU);
