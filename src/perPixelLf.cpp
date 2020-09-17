d0ae53566f8af3ba3d959d87f7a2e7c06f65cebf
ichlubna <ichlubna@fit.vutbr.cz>
2020-09-17 11:50:37 +0200

    Per pixel LF tool prototype

---
+++ src/perPixelLf.cpp
@@ -0,0 +1,125 @@
+#include <fstream>
+#include <iostream>
+#include <vector>
+#include <filesystem>
+#include <algorithm>
+#include <functional>
+#include <cmath>
+#include <ArgumentViewer/ArgumentViewer.h>
+
+std::pair<int, int> getGridSize(std::string path)
+{
+    std::vector<std::filesystem::path> files;
+    for (const auto & file : std::filesystem::directory_iterator(path))
+        if(file.path().extension() == ".ppm")
+            files.push_back(file.path().filename());
+    std::sort(files.begin(), files.end(), [](const auto& lhs, const auto& rhs)
+             {return lhs.string() < rhs.string();});
+
+    auto lastName = files.back().string();
+    auto delimiter = lastName.find("_");
+    return {std::stoi(lastName.substr(0,delimiter)), std::stoi(lastName.substr(delimiter+1))};
+}
+
+void process(int argc, char **argv)
+{
+    auto args = argumentViewer::ArgumentViewer(argc,argv);
+    auto inputFolder =  args.gets("-i","","input folder with images in format x_y.ppm starting with 0_0");  
+    auto outputFolder =  args.gets("-o","","output folder");
+    auto const interlace = args.getu32("-n",1,"interlace factor");
+    if(inputFolder.empty() || outputFolder.empty())
+        throw args.toStr();
+
+    if (inputFolder.back() != '/')
+        inputFolder.push_back('/');  
+    if (outputFolder.back() != '/')
+        outputFolder.push_back('/');  
+  
+    auto gridSize = getGridSize(inputFolder); 
+    
+    std::vector<std::vector<std::ifstream>> ifstreams;
+    std::pair<int, int> imageResolution;
+    int maxValue = 255; 
+    for(int x=0; x<gridSize.first; x++)
+    {
+        ifstreams.push_back({});
+        for(int y=0; y<gridSize.second; y++)
+        { 
+            ifstreams[x].push_back(std::ifstream(inputFolder+std::to_string(x)+"_"+std::to_string(y)+".ppm", std::ios::in | std::ios::binary));
+            std::string line;
+            std::getline(ifstreams[x][y], line);
+            if (line != "P6") throw("Input ppm not in binary format!");
+            do std::getline(ifstreams[x][y], line);
+            while (line[0] == '#');
+            std::stringstream imgSize(line);    
+            try
+            {
+                imgSize >> imageResolution.first;
+                imgSize >> imageResolution.second;
+            }
+            catch (...)
+            {
+                throw("Input ppm error: no resolution");
+            }
+                
+            do std::getline(ifstreams[x][y], line);
+            while (line[0] == '#');
+            std::stringstream maxValSS(line);   
+            try
+            {
+                maxValSS >> maxValue;
+            }
+            catch (...)
+            {
+                throw("Input ppm error: no max value");
+            }
+        }}
+
+    int bitdepth = std::log2(maxValue+1);
+    std::cout << "Grid size: " << gridSize.first << " " << gridSize.second << std::endl;
+    std::cout << "Resolution: " << imageResolution.first << " " << imageResolution.second << std::endl;
+    std::cout << "Bitdepth: " << bitdepth << std::endl;
+    if(bitdepth != 8)
+        throw("8-bit only support");
+    
+    std::ofstream outputFile;
+    int i=0;
+    std::vector<char> buffer(3,0);
+    for(int x=0; x<imageResolution.first; x++)
+        for(int y=0; y<imageResolution.second; y++)
+        {
+            if(i % interlace == 0)
+            {
+                outputFile.close();
+                outputFile.open(outputFolder+std::to_string(x)+"_"+std::to_string(y)+".ppm");
+                outputFile << "P6" << std::endl;
+                outputFile << gridSize.first*interlace << " " << gridSize.second*interlace << std::endl;
+                outputFile << std::pow(2, bitdepth)-1 << std::endl << std::endl;
+            }
+            for (auto &ifss : ifstreams)
+                for(auto &ifs : ifss)
+                {
+                   //TODO interlace to read more from one stream to have same cam pixels grouped
+                   ifs.read(buffer.data(), buffer.size()); 
+                   outputFile.write(buffer.data(), buffer.size());;
+                }
+        }
+}
+
+void reconstruct(int x, int y, int interlace)
+{
+
+}
+
+int main (int argc, char **argv)
+{
+    try{
+        process(argc, argv);    
+    }
+    catch (const std::exception& e)
+    {
+        std::cerr << e.what() << std::endl;
+        return EXIT_FAILURE;
+    }
+    return EXIT_SUCCESS;
+}
