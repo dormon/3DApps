2a9c1fd656c3c4c4a969ea8fe874bf56652331ca
ichlubna <43234438+ichlubna@users.noreply.github.com>
2021-03-10 08:18:12 +0100

    Quaternion

--- src/utils/ORB_SLAM3Analyzer.cpp
+++ src/utils/ORB_SLAM3Analyzer.cpp
@@ -3,45 +3,7 @@
 #include <opencv2/opencv.hpp>
 #include <string>
 #include "System.h"
-
-cv::Mat rot2euler(const cv::Mat & rotationMatrix)
-{
-    cv::Mat euler(3,1,CV_64F);
-
-    double m00 = rotationMatrix.at<double>(0,0);
-    double m02 = rotationMatrix.at<double>(0,2);
-    double m10 = rotationMatrix.at<double>(1,0);
-    double m11 = rotationMatrix.at<double>(1,1);
-    double m12 = rotationMatrix.at<double>(1,2);
-    double m20 = rotationMatrix.at<double>(2,0);
-    double m22 = rotationMatrix.at<double>(2,2);
-
-    double bank, attitude, heading;
-
-    // Assuming the angles are in radians.
-    if (m10 > 0.998) { // singularity at north pole
-        bank = 0;
-        attitude = CV_PI/2;
-        heading = atan2(m02,m22);
-    }
-    else if (m10 < -0.998) { // singularity at south pole
-        bank = 0;
-        attitude = -CV_PI/2;
-        heading = atan2(m02,m22);
-    }
-    else
-    {
-        bank = atan2(-m12,m11);
-        attitude = asin(m10);
-        heading = atan2(-m20,m00);
-    }
-
-    euler.at<double>(0) = bank;
-    euler.at<double>(1) = attitude;
-    euler.at<double>(2) = heading;
-
-    return euler;
-}
+#include "Converter.h"
 
 int main(int argc, char **argv)
 {
@@ -73,8 +35,8 @@ int main(int argc, char **argv)
         {
             cv::Rect rect(0,0,3,3);
             cv::Mat rot = mat(rect);
-            auto rotation = rot2euler(rot);
-            ofs << i << " " << mat.at<float>(0,3) << " " << mat.at<float>(1,3) << " " << mat.at<float>(2,3) << " " << rotation.at<float>(0,0) << " " << rotation.at<float>(0,1) << " " << rotation.at<float>(0,2) << std::endl;
+            auto rotation = ORB_SLAM3::Converter::toQuaternion(rot);
+            ofs << i << " " << mat.at<float>(0,3) << " " << mat.at<float>(1,3) << " " << mat.at<float>(2,3) << " " << rotation[0] << " " << rotation[1] << " " << rotation[2] << " " << rotation[3] << std::endl;
         }
         i++;
     }
